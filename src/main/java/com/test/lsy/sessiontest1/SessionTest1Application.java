0dc255f1e4ef7c6127565c74d640b0a78633e0b8
이상엽 <infightersy@naver.com>
2025-03-04 16:13:02 +0900

    레디스 세션 공유 예제

---
+++ src/main/java/com/test/lsy/sessiontest1/SessionTest1Application.java
@@ -0,0 +1,13 @@
+package com.test.lsy.sessiontest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/lsy/sessiontest1/SessionTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.lsy.sessiontest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
